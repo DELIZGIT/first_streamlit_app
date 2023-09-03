aa5183dff5412f7fe2795549c95acc031802d64a
DELIZGIT <100713751+DELIZGIT@users.noreply.github.com>
2023-09-03 14:48:40 -0600

    Update streamlit_app.py
    Added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & blueberry Oatmeal')
+streamlit.text('kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled free-range egg')
